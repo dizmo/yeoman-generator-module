71e27730437b07a0b7a6ee3f258d96e9711f415b
Hasan Karahan <hasan.karahan@blackhan.com>
2020-05-14 14:32:45 +0300

    DIG-304: npm run docs w/jsdoc on {dist=>lib}

--- generators/app/templates/cli/run-docs.js
+++ generators/app/templates/cli/run-docs.js
@@ -2,12 +2,11 @@ const { npm, npx } = require('./run-utils');
 const { exit } = require('process');
 
 const jsdoc = () => npx('jsdoc',
-    '--configure', 'jsdoc.json', 'dist/lib/index.js'
+    '--configure', 'jsdoc.json'
 );
 if (require.main === module) {
     npm('install')
         .then(() => require('./run-clean')('docs'))
-        .then(() => require('./run-build')())
         .then(jsdoc).catch(exit);
 }
 module.exports = jsdoc;
