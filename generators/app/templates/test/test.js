79a7d490d889b00e3cdf2065ab3f251bb88b7be0
Hasan Karahan <hasan.karahan@blackhan.com>
2021-03-10 18:59:41 +0300

    DIG-385: tweaked npm run test

--- generators/app/templates/test/test.js
+++ generators/app/templates/test/test.js
@@ -1,33 +1,33 @@
 /* eslint-env browser, node, mocha */
-import * as index from '../lib';
 import { expect } from 'chai';
+import { id } from '../lib';
 
 describe('index.id', () => {
     it('should exist', () => {
-        expect(index).to.not.be.an('undefined');
+        expect(id).to.not.be.an('undefined');
     });
     it('should be a function', () => {
-        expect(index.id).to.be.a('function');
+        expect(id).to.be.a('function');
     });
 });
 
 describe('index.id', () => {
     it('should return nothing', () => {
-        expect(index.id()).to.equal(undefined);
+        expect(id()).to.equal(undefined);
     });
     it('should return a value', () => {
-        expect(index.id(undefined)).to.equal(undefined);
-        expect(index.id(null)).to.equal(null);
-        expect(index.id(true)).to.equal(true);
+        expect(id(undefined)).to.equal(undefined);
+        expect(id(null)).to.equal(null);
+        expect(id(true)).to.equal(true);
     });
     it('should return an array', () => {
-        let a = index.id(undefined, null)
+        let a = id(undefined, null)
         expect(a).to.be.an('array');
         expect(a).to.include.members([undefined, null]);
-        let b = index.id(false, true)
+        let b = id(false, true)
         expect(b).to.be.an('array');
         expect(b).to.include.members([false, true]);
-        let c = index.id(0, 1)
+        let c = id(0, 1)
         expect(c).to.be.an('array');
         expect(c).to.include.members([0, 1]);
     });
